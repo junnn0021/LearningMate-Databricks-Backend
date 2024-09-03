4d59be036a2fa2ef600c5bb6c1597734615e199f
EC2 Default User <ec2-user@ip-172-31-1-153.ap-northeast-2.compute.internal>
2024-09-03 04:45:16 +0000

    DB 연결 테스트

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from typing import Union
- 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+from datetime import date
+import pymysql
+import os
  
 app = FastAPI()
  
  
 @app.get("/")
 def read_root():
-    return {"Hello": "Joonn2 World"}
+    return {"Hello": "Junseok World"}
+
+
+mysql_host = os.environ['MYSQL_HOST']
+mysql_user = os.environ['MYSQL_USER']
+mysql_password = os.environ['MYSQL_PASSWORD']
+mysql_database = os.environ['MYSQL_DATABASE']
+
+def get_db_connection():
+    connection = pymysql.connect(
+        host=mysql_host,
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_database
+    )
+    return connection
+
+@app.get("/test-db")
+def test_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM new_table")
+    result = cursor.fetchall()
+    connection.close()
+
+    result = [(str(row[0]), row[1]) for row in result]
+
+    return JSONResponse(content={"message": "Database connection successful", "result": result})
