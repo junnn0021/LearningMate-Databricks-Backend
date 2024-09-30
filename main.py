1d63911058c065b7eb3e742229533961b392107a
junnn0021 <junnn0021@gmail.com>
2024-09-30 06:32:44 +0000

    create문에 hire 컬럼 추가

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ def test_db():
     return JSONResponse(content={"message": "Database connection successful", "result": result})
     
 @app.post("/items")
-async def create_item(id: int, first_name: str, last_name: str, email: str):
+async def create_item(id: int, first_name: str, last_name: str, email: str, hire: str):
     connection = get_db_connection()
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO new_table (id, first_name, last_name, email) VALUES (%s, %s, %s, %s)", (id, first_name, last_name, email))
+    cursor.execute("INSERT INTO new_table (id, first_name, last_name, email, hire) VALUES (%s, %s, %s, %s, %s)", (id, first_name, last_name, email, hire))
     connection.commit()
     connection.close()
     return JSONResponse(content={"message": "Item created successfully"}, status_code=201)
